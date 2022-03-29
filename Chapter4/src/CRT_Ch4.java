efb39b4bd1f18ad4e388b313f0a18321aa3530f3
104361003 <104361003@L815WD220004.edu.cbe.ab.ca>
2022-03-29 10:55:25 -0600

    done

---
+++ Chapter4/src/CRT_Ch4.java
@@ -0,0 +1,119 @@
+import java.util.Scanner;
+
+public class CRT_Ch4 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		//pg 92 1-6
+	/*	
+		1a) if (grade > 90)
+	       {
+	           System.out.println("Great Job!!!");
+
+	b)   if (grade < 20)
+	       {
+	           System.out.println("Error");
+	       }
+	       else if (grade > 50)
+	       {
+	        System.out.println("Error");
+	       }
+	c)	 System.out.println("Enter number: ");
+       int y = userinput.nextInt();
+       	if (y<100)
+          	{
+        	   int y2 = y +2
+        	   System.out.println(“The value of your number is now ” + y2);
+        	 } 
+
+	2. Scanner userinput = new Scanner(System.in);
+       System.out.println("Enter number: ");
+       int y = userinput.nextInt();
+  	System.out.println("Enter second number: ");
+       int x = userinput.nextInt();
+   	input.close();
+       	if(y>x)
+       	{
+           	System.out.println("First number is larger. ");
+       	}
+       	if(x>y){
+       	
+       		System.out.println("the second number is larger");
+       	
+       	}
+       	if( y == x){
+       	
+       		System.out.println("the numbers are equal");
+       	
+       	}
+
+	 
+	3.a)	if(num % 2 == 0){
+	
+		System.out.println("Even number");
+	
+	} else {
+	
+		System.out.println("Odd number");
+	
+	}
+	b)
+
+
+		Scanner input = new Scanner(System.in);
+		
+		System.out.println("input a number");
+		
+		int num = input.nextInt();
+		
+		int number = (num % 2);
+		
+		switch(number) {
+		
+		case 0: 
+			System.out.println("Even number");
+		break;
+		
+		case 1: 
+			System.out.println("Odd number");
+		break;
+		
+		
+		
+		}
+		
+		
+		
+		
+	
+
+	4a) 
+
+		
+	int random = (int)(Math.random() * 50 + 1);
+	   System.out.println(random);
+	   
+	
+	//4b) 
+	
+		int random = (int)(Math.random() * (100 - 20) + 20);
+		   System.out.println(random);
+	
+	
+//	4c)
+
+		double random = (double)(Math.random() * (20 - 10) + 10);
+		   System.out.println(random);
+		
+		
+	5.	if the user inputs 65 or 18 there wont be an answer, and negative numbers will give children
+	
+	
+	6. a. true b. false c. false d. false e. true f. true g. true
+		
+		
+*/		
+	}
+
+}
