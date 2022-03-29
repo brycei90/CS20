a2aad5ada7f59e15aba541fd28dc1d690f4314b5
104361003 <104361003@L815WD220004.edu.cbe.ab.ca>
2022-03-29 11:53:11 -0600

    not done

---
+++ Chapter6/src/Ex3ch6.java
@@ -0,0 +1,38 @@
+
+import java.util.Scanner;
+/*
+
+Program:.java          Last Date of this Revision: February 24, 2022
+ 
+Purpose: modify the prime number application created in chapter 5 exercise 1 to include a method named isPrime().
+
+Author: Bryce Ilcan
+School: CHHS
+Course: Computer Programming 20
+ 
+ 
+*/
+public class Ex3ch6 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Scanner num = new Scanner(System.in);
+		
+		System.out.println("Please input a number");
+		
+		int n = num.nextInt();
+		
+		//prime numbers are evenly divisble by itself and 1.
+		
+		
+		
+		
+		
+
+	}
+
+}
+
+
+
---
+++ Chapter6/src/TimeConverter.java
@@ -0,0 +1,84 @@
+/*
+Program:.java          Last Date of this Revision: March 29, 2022
+ 
+Purpose: a time converter application that allows the user to choose among converting hours to minutes, days to hours, minutes to hours, or hours to days.
+
+Author: Bryce Ilcan
+School: CHHS
+Course: Computer Programming 20
+ 
+ 
+*/
+
+import java.util.Scanner;
+
+public class TimeConverter {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Scanner input = new Scanner(System.in);
+		
+		System.out.println("Would you like hour to minutes, days to hours, minutes to hours, or hours to days");
+		
+		String TT = input.nextLine();
+		
+		switch(TT) {
+		
+		case ("hour to minutes"): 
+			Scanner userinput = new Scanner(System.in);
+		
+			System.out.println("please input your hours");
+		
+			int hour = userinput.nextInt();
+		
+			int mins = hour * 60;
+		
+			System.out.println(hour + " hours in minutes is: " + mins + " minutes");
+		break;
+		
+		case ("days to hours"): 
+			Scanner userinput2 = new Scanner(System.in);
+		
+			System.out.println("please input your days");
+		
+			int hour2 = userinput2.nextInt();
+		
+			int mins2 = hour2 * 60;
+		
+			System.out.println(hour2 + " days in hours is: " + mins2 + " hours");
+		break;
+		
+		case ("minutes to hours"): 
+			Scanner userinput3 = new Scanner(System.in);
+		
+			System.out.println("please input your minutes");
+		
+			double hour3 = userinput3.nextDouble();
+		
+			double mins3 = hour3 / 60;
+		
+			System.out.println(hour3 + " minutes in hours is: " + mins3 + " hours");
+		break;
+		
+		case ("hours to days"): 
+			Scanner userinput4 = new Scanner(System.in);
+		
+			System.out.println("please input your hours");
+		
+			double hour4 = userinput4.nextDouble();
+		
+			double mins4 = hour4 / 24;
+		
+			System.out.println(hour4 + " hours in days is: " + mins4 + " days");
+		break;
+		
+		}
+		
+		
+		
+		
+		
+	}
+
+}
