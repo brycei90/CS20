85d640663ea30aea4971ff40503299df1d6da566
104361003 <104361003@L815WD220004.edu.cbe.ab.ca>
2022-04-21 11:59:06 -0600

    working on adder

--- Chapter7/src/Adder.java
+++ Chapter7/src/Adder.java
@@ -14,24 +14,69 @@ Course: Computer Programming 20
  
 */
 import java.awt.BorderLayout;
+import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.Random;
+import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.JTextField;
 
 class Adder implements ActionListener{
 
 	 public static JFrame frame = new JFrame("Adder!");
      public static JPanel panel = new JPanel();
      public static JPanel panel2 = new JPanel();
+     public static JPanel panel3 = new JPanel();
 	
-	
+	 JLabel questions = new JLabel(" ");
+	 JButton sub = new JButton("submit.");
+	 JTextField res = new JTextField(5);
+     JLabel rW = new JLabel(" ");
+	 
+	 int resp, num1, num2;
+	 
 	public Adder(){
 		
+		//GridLayout layout = new GridLayout(5,1);
+
+		//panel.setLayout(layout);
 		
+		panel.add(res);
+		panel.add(sub);
+		panel2.add(questions);
+		panel3.add(rW);
+		
+		num1 = (int )(Math.random() * 20 + 1);
+		num2 = (int )(Math.random() * 20 + 1);
+	
+	}
+	
+	
+	@Override
+	public void actionPerformed(ActionEvent e) {
+	
+		if(e.getSource() == sub) {
+			
+			resp = Integer.parseInt(res.getText());
+			
+			if((num1 + num2) == resp) {
+				
+				rW.setText("correct!");
+				
+			}else if((num1 + num2) != resp){
+				
+				rW.setText("Wrong answer! " + answer);
+				
+			}
+			
+			
+		}
 		
 		
 	}
-	
 	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
@@ -42,7 +87,8 @@ class Adder implements ActionListener{
  		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setSize(700, 400);
         frame.getContentPane().add(BorderLayout.EAST, panel);
-        frame.getContentPane().add(BorderLayout.CENTER, panel2);
+        frame.getContentPane().add(BorderLayout.WEST, panel2);
+        frame.getContentPane().add(BorderLayout.CENTER, panel3);
         frame.setVisible(true);
 		
 	}
