20d49248fa690019c084b381a080b150aadf66aa
104361003 <104361003@L815WD220004.edu.cbe.ab.ca>
2022-04-26 11:23:22 -0600

    done!

--- Chapter7/src/CRTpg175.java
+++ Chapter7/src/CRTpg175.java
@@ -1,4 +1,37 @@
 
 public class CRTpg175 {
+//1-6
+	/*
+1. Radius(), Circumference(), Area(), Circle()
+2. The constructor must have the same name as the class.
+3. Public can be accessed from the child class of the 
+same package whereas the Private cannot.
+4. yes, but only if the constructor for circle class accepts an integer and 
+has a public integer field called radius.
+5. 
+a) Roo
+b) Public
+c) GetX
+d) SetX
+e) factor
+f) Roo
+g) 1
+6. Class is a blueprint or template from which objects are created, 
+and objects are an instance of a class.
 
+
+
+
+
+
+
+
+
+
+
+	 */
+	
+	
+	
+	
 }
