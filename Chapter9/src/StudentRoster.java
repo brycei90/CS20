a763ce9189948db32a8742b69913a5381142727b
104361003 <104361003@L815WD220004.edu.cbe.ab.ca>
2022-04-26 11:59:47 -0600

    student roster

--- Chapter9/src/StudentRoster.java
+++ Chapter9/src/StudentRoster.java
@@ -1,9 +1,33 @@
+/*
+
+Program:.java          Last Date of this Revision: April 26, 2022
+ 
+Purpose: prompts the user for the number of students in the class and then prompts the user for each students name and stores the names in an array. after the names have been entered,
+the application should display the title then list the names in the array!
+
+Author: Bryce Ilcan
+School: CHHS
+Course: Computer Programming 20
+ 
+ 
+*/
+import java.util.Scanner;
 
 public class StudentRoster {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		Scanner userinput = new Scanner(System.in);
+		
+		System.out.println("How many students are in your class");
+		
+		int studentA = userinput.nextInt();
+		
+		String[] studentN = new String[studentA]; 
+		
+		System.out.println("Please input all the students names: ");
 
 	}
 
 }
+
