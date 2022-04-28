b85e5b6ef5bc5ffec62800184c44f8af096bcb02
104361003 <104361003@L815WD220004.edu.cbe.ab.ca>
2022-04-28 11:45:14 -0600

    on dice rolls

--- Chapter9/src/Squares.java
+++ Chapter9/src/Squares.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 
 /*
 
-Program:.java          Last Date of this Revision: April 27, 2022
+Program:.java          Last Date of this Revision: April 28, 2022
  
 Purpose: stores the square of an elements index in an integer array of 5 elements for example, the third element, which has index 2, should store 4. the application should then display
 the value of each element in the array!
@@ -17,34 +17,45 @@ public class Squares {
 
 	public static void main(String[] args) 
 	{
-		
+		//stores number
 		Scanner userinput = new Scanner(System.in);
 		
 		System.out.println("Please input a number that is greater than or equal to 5!");
 		
 		int num = userinput.nextInt();
 		
+		int x = num - 5;//creates the array so it has 5 elements.
+		
+		int squareNumbers = num * num;//square the input number
+		
+		int[] numbers = new int[squareNumbers];
 		
 		
-		//stores names
-		for(int i = 0; i < studentA; i++)
+		for(int i = x; i < num; i++) 
 		{
-			Scanner Uinput = new Scanner(System.in);
-			System.out.println("Please input all the students names: ");
-			String names = Uinput.next();
-			studentN[i] = names;
-			
+			numbers[i] = i;	
 		}
 		
-		System.out.println("Student Roster: ");
+		//displays count down
+		System.out.println("the square of all the numbers before your inputted number is: ");
 		
-		//displays names
-		for(int i = 0; i < studentA; i++)
-		{
-			System.out.println(studentN[i]);
+		for(int i = x; i < num; i++)
+		{	
+			System.out.println(numbers[i] * numbers[i]);	
+		
 		}
-		
-		
-	}
 
+	}
 }
+/* screen dump
+
+Please input a number that is greater than or equal to 5!
+6
+the square of all the numbers before your inputted number is:  
+1
+4
+9
+16
+25
+
+*/
---
+++ Chapter9/src/reverse.java
@@ -0,0 +1,102 @@
+import java.awt.BorderLayout;
+import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+
+/*
+
+Program:.java          Last Date of this Revision: April 28, 2022
+ 
+Purpose: application that stores the number corresponding to the elements index in an integer array of 10 elements. for example, the second element, which as index 1 should store 1.
+the application should then display the title count down and then list numbers stored in the array in reverse order.
+
+Author: Bryce Ilcan
+School: CHHS
+Course: Computer Programming 20
+ 
+ 
+*/
+public class reverse implements ActionListener{
+
+	//creating frame and components
+	public static JFrame frame = new JFrame("reverse.");
+    public static JPanel panel = new JPanel();
+    public static JPanel panel2 = new JPanel();
+	
+    JButton sub = new JButton("Submit");
+    JTextField TF = new JTextField(5);
+	JLabel title = new JLabel("Input a number greater than 10: ");
+    JLabel list1 = new JLabel(" ");
+    JTextField list2 = new JTextField(" ");
+	
+    //declaring variables
+    int x, num, i, numbers, numb;
+    
+	public reverse(){
+		
+		//adding action listener(s)
+		sub.addActionListener(this);
+		
+    	//the layout that I am using
+		GridLayout layout = new GridLayout(3,2);
+		panel.setLayout(layout);
+		panel2.setLayout(layout);
+		panel.add(title);
+		panel.add(TF);
+		panel.add(sub);
+		panel2.add(list1);
+		panel2.add(list2);
+		
+	}
+	
+	public void actionPerformed(ActionEvent e) {
+		
+		if(e.getSource() == sub)
+		{	
+				
+			
+			num = Integer.parseInt(TF.getText());
+			
+			x = num - 10;
+			
+			int[] numbers = new int[num];
+			
+			
+			
+			for(i = 0; i < num; i++) 
+			{
+				numbers[i] = i;	
+			}
+			
+			//displays count down
+			list1.setText("CountDown: ");
+			
+			for(i = num - 1; i >= x; i--)
+			{	
+				list2.setText(list2.getText() + "\n" + numbers[i]);	
+			}
+			
+		}
+		
+	}
+	
+	
+	public static void main(String[] args) {
+		
+		reverse r = new reverse();
+
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(600, 500);
+        frame.getContentPane().add(BorderLayout.EAST, panel);
+        frame.getContentPane().add(BorderLayout.WEST, panel2);
+        frame.setVisible(true);
+		
+	}
+
+}
