9e7576b7df34b7381939fdb82cce0209fba3f0a5
104361003 <104361003@L815WD220004.edu.cbe.ab.ca>
2022-05-06 09:28:21 -0600

    done!

---
+++ Chapter9/src/CRTpg235_1to4.java
@@ -0,0 +1,63 @@
+//import java.util.Scanner;
+
+public class CRTpg235_1to4 {
+
+	/*
+	
+	1. 3.
+	
+	2. int[] quantities = new int[20];
+	
+	3. double[] heights = {1.65, 2.15, 4.95}; 
+	
+	4. 	
+	for(int i = 0; i < grades; i++) 
+	{
+		grade[i] = i;
+	}
+	for(int i = 0; i < grades; i++)
+	{
+		System.out.print("grade: " + " " + grade[i] + ", ");
+	}
+	
+	*/
+	
+	
+/*public static void main(String[] args) {
+	
+	Scanner input = new Scanner(System.in);
+	System.out.println("input your grade!");
+	int grades = input.nextInt();
+	
+	int[] grade = new int[grades];
+	
+	for(int i = 0; i < grades; i++) 
+	{
+		grade[i] = i;
+	}
+	for(int i = 0; i < grades; i++)
+	{
+		System.out.print("grade: " + " " + grade[i] + ", ");
+	}
+
+	}
+	
+	
+*/
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+}
