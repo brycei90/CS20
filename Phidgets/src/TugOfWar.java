ae0ece27b092b1e5f649936cdb1b916bdc742064
104361003 <104361003@L815WD220004.edu.cbe.ab.ca>
2022-05-18 09:32:10 -0600

    tugOFwar

--- Phidgets/src/TugOfWar.java
+++ Phidgets/src/TugOfWar.java
@@ -2,7 +2,8 @@ import com.phidget22.DigitalInput;
 import com.phidget22.DigitalOutput;
 
 public class TugOfWar {
-	    public static void main(String[] args) throws Exception{
+	    public static void main(String[] args) throws Exception
+	    {
 
 	    }
 }
