080af196b00a503cdf6e4d09b5ff014a5861f163
104361003 <104361003@L815WD220004.edu.cbe.ab.ca>
2022-05-19 11:17:39 -0600

    !

--- GettingStarted/src/GettingStarted.java
+++ GettingStarted/src/GettingStarted.java
@@ -5,56 +5,27 @@ import com.phidget22.*;
 public class GettingStarted {
     //Handle Exceptions 
     public static void main(String[] args) throws Exception{
-
-    	int P = 0;
-    	
+        
         //Create 
         DigitalInput redButton = new DigitalInput();
-        DigitalOutput redLED = new DigitalOutput();
-        DigitalInput greenButton = new DigitalInput();
-        DigitalOutput greenLED = new DigitalOutput();
+        boolean STATE = false;
 
         //Address 
         redButton.setHubPort(0);
         redButton.setIsHubPortDevice(true);
-        redLED.setHubPort(1);
-        redLED.setIsHubPortDevice(true);
-        greenButton.setHubPort(5);
-        greenButton.setIsHubPortDevice(true);
-        greenLED.setHubPort(4);
-        greenLED.setIsHubPortDevice(true);
 
         //Open 
         redButton.open(1000);
-        redLED.open(1000);
-        greenButton.open(1000);
-        greenLED.open(1000);
 
         //Use your Phidgets 
         while(true){
-
-         /*   if(redButton.getState()){
-                greenLED.setState(true);
-                P += 1;
-            } else {
-                greenLED.setState(false);
-            }
-
-            if(greenButton.getState()){
-                redLED.setState(true);
-                P += 1;
-            } else {
-                redLED.setState(false);       
-            }
-            System.out.println(" " + P);
-            Thread.sleep(150);*/
-        if(redButton.getState() == true)
-        {
-        	System.out.println("Button State: " + redButton.getState());
-        	Thread.sleep(150);
-        }
+        	
+        	if(redButton.getState() != STATE)
+        	{
+        		System.out.println("Button State: " + redButton.getState());
+        		STATE = redButton.getState();
+        	}
+            Thread.sleep(100);
         }
     }
 }
-
-
