1b543c156629127ee4f0ee264af32b610fe8a84d
104361003 <104361003@L815WD220004.edu.cbe.ab.ca>
2022-05-24 11:48:44 -0600

    hotorcold

---
+++ Phidgets/src/HotOrCold.java
@@ -0,0 +1,44 @@
+import com.phidget22.DigitalInput;
+import com.phidget22.DigitalOutput;
+import com.phidget22.TemperatureSensor;
+
+public class HotOrCold {
+
+	public static void main(String[] args) throws Exception
+	{
+		double F = 0, T = 0;
+		//Create 
+        TemperatureSensor temperatureSensor = new TemperatureSensor();
+        DigitalOutput redLED = new DigitalOutput();
+        DigitalOutput greenLED = new DigitalOutput();
+        
+        //address
+        redLED.setHubPort(0);
+        redLED.setIsHubPortDevice(true);
+        greenLED.setHubPort(0);
+        greenLED.setIsHubPortDevice(true);
+        
+        //Open 
+        temperatureSensor.open(1000);
+        redLED.open(1000);
+        greenLED.open(1000);
+        //F = (temperatureSensor.getTemperature() * 1.8) + 32;
+        
+        //Use your Phidgets 
+        while (true) {
+        	T = temperatureSensor.getTemperature();
+        	
+        	if((T > 20)&&(T < 24))
+        	{
+        		greenLED.setState(true);
+        	}
+        	else
+        	{
+        		redLED.setState(true);
+        	}
+            System.out.println("Temperature: " + temperatureSensor.getTemperature() + " °C" );
+            Thread.sleep(150);
+        }
+	}
+
+}
